06d588507c8f5af32906941848402b8bc0ffde34
Zoemchl <71171434+Zoemchl@users.noreply.github.com>
2024-11-23 17:42:22 +0100

    navbar

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+"use client";
+import styles from "./navbar.component.module.css";
+
+export default function NavbarComponent(){
+
+  return (
+    <div className={styles.topnav}>
+      <ul>
+          <a href="#about">About</a>
+          <a href="#team">Team</a>
+          <a href="#threed">3D Visualization</a>
+      </ul>
+    </div>
+  );
+}
